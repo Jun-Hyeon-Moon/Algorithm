300a0ac089320898e3263bfbd547862fd232fa17
JunHyeon <vendrick0526@gmail.com>
2024-03-06 22:58:58 +0900

    2024_03_26 내적.js

---
+++ Programmers/Level1/3월/내적.js
@@ -0,0 +1,20 @@
+//문제설명
+/*
+길이가 같은 두 1차원 정수 배열 a, b가 매개변수로 주어집니다.
+a와 b의 내적을 return 하도록 solution 함수를 완성해주세요.
+이때, a와 b의 내적은 a[0]*b[0] + a[1]*b[1] + ... + a[n-1]*b[n-1] 입니다. (n은 a, b의 길이)
+*/
+
+//내 솔루션
+function solution(a, b) {
+    var ans=0;
+    for(i=0; i<a.length; i++){
+        ans+=a[i]*b[i];
+    }
+    return ans;
+}
+
+//다른 분 솔루션
+function solution(a, b) {
+    return a.reduce((acc, _, i) => acc += a[i] * b[i], 0);
+}
